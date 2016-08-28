e7784f437d2d890ffab234f92a2e35a094a27c42
oleksandr <dr.newman.ua@gmail.com>
2016-08-28 20:49:28 +0300

    Initial commit

---
+++ src/com/netcracker/edu/Main.java
@@ -0,0 +1,13 @@
+package com.netcracker.edu;
+
+import java.util.Objects;
+
+public class Main {
+
+    public static void main(String[] args) throws ClassNotFoundException {
+	// write your code here
+        Class clazz = Object.class;
+        Object object = new Object();
+        "tyest".compareTo("tyest");
+    }
+}
